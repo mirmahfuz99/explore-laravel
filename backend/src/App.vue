28ceb8bad8eb3418c6b3240872bfeb95c120505e
mir10 <mirmahfuz10@gmail.com>
2023-08-20 22:46:48 +0600

    route feature implemented

--- backend/src/App.vue
+++ backend/src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
 
-    <img alt="Vue logo"  src="@/assets/logo.svg" />
-    <HelloWorld msg="You did it!" />
+<router-view></router-view>
 
 </template>
 
 
+<style>
+
+</style>
--- backend/src/main.js
+++ backend/src/main.js
@@ -8,5 +8,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
