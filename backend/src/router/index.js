a88058eee9f082566cd72cd6efdf5f88ccf02cde
mir10 <mirmahfuz10@gmail.com>
2023-08-21 22:29:14 +0600

    implementing guest layouts features

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
 import Dashboard from '../views/Dashboard.vue';
 
+import RequestPassword from '../views/RequestPassword.vue';
+import ResetPassword from '../views/ResetPassword.vue';
+
 
 
 const router = createRouter({
@@ -11,14 +14,22 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
-
       component: Dashboard
     },
     {
       path: '/login',
       name: 'login',
-
       component: Login
+    },
+    {
+      path: '/request-password',
+      name: 'requestPassword',
+      component: RequestPassword
+    },
+    {
+      path: '/reset-password',
+      name: 'resetPassword',
+      component: ResetPassword
     }
   ]
 })
