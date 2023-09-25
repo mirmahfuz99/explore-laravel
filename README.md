fb582d63eb03487e3ae805a953131b27331d395d
mir10 <mirmahfuz10@gmail.com>
2023-09-25 09:43:41 +0600

    laravel-vue-ecommerce updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-https://www.youtube.com/watch?v=jffKw_NMfnw
+https://www.youtube.com/watch?v=jffKw_NMfnw&t=1974s
+
+<!-- create vue project -->
+npm create vite@latest
+
+<!-- install tailwindcss  -->
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p 
+
+<!-- install vuex next -->
+npm install vuex@next
+
+<!-- install vue router -->
+npm install -S vue-router@next
+
+<!-- install headlessui, heroicons and tailwindcss forms -->
+npm install -D @headlessui/vue @heroicons/vue @tailwindcss/forms
--- database/migrations/2023_08_18_212649_create_products_table.php
+++ database/migrations/2023_08_18_212649_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image')->nullable();
+            $table->string('image_mime')->nullable();
+            $table->string('image_size')->nullable();
+            $table->string('description')->nullable();
+            $table->decimal('price', 10,2);
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable();
+            $table->softDeletes();
+            $table->foreignIdFor(User::class, 'deleted_by')->nullable();
             $table->timestamps();
         });
     }
